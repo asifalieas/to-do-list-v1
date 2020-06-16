3faf369ea1a0ca6053b098d973fce54aaf58744e
unknown <asifalieas@gmail.com>
2020-06-16 10:45:16 +0100

    initial commit

---
+++ date.js
@@ -0,0 +1,12 @@
+// jshint esversion:6
+
+exports.getDate = function() {
+  let today = new Date();
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  return today.toLocaleDateString("en-us", options);
+}
